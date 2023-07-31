68673ed5ef070c3085141d18fd652a95d2acf3e3
Satishpokala124 <satishpokala124@gmail.com>
2023-08-01 00:07:14 +0530

    ✅ Test case for Label [skip ci]

---
+++ src/components/Label/Label.stories.tsx
@@ -0,0 +1,17 @@
+import { Label } from './Label';
+
+export default {
+  title: 'Label',
+  component: Label,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export const Default = {
+  args: {
+    text: 'Label',
+  },
+};
